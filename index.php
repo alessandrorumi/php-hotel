dde32bf97eb10bec907fc708deb4cd90d2bcbfd6
alessandrorumi <rumi.alessandro21@gmail.com>
2024-01-23 17:37:25 +0100

    Add: Filter

--- index.php
+++ index.php
@@ -57,59 +57,96 @@
     // }
   ?>
 
-    <form>
-    <div class="form-row">
-      <div class="form-group col-md-2 my-3">
-        <label for="parking">Parcheggio</label>
-        <select id="parking" name="parking" class="form-select">
-          <option value="">Tutti</option>
-          <option value="1">Con Parcheggio</option>
-          <option value="2">Senza Parcheggio</option>
-        </select>
-      </div>
-      <div class="form-group col-md-2 mb-1">
-        <label for="vote">Voto</label>
-        <select id="vote" name="vote" class="form-select">
-          <option value="">Tutti</option>
-          <option value="1">2 Stelle e superiori</option>
-          <option value="2">3 Stelle e superiori</option>
-          <option value="3">4 Stelle e superiori</option>
-          <option value="4">5 Stelle e superiori</option>
-        </select>
-      </div>
-      <div class="col-auto">
-        <button type="submit" class="btn btn-primary mb-3">Filtra</button>
-      </div>
-    </form>
+  <form method="GET">
+  <div class="form-row">
+    <div class="form-group col-md-2 my-3">
+      <label for="parking">Parcheggio</label>
+      <select id="parking" name="parking" class="form-control">
+        <option value="">Tutti</option>
+        <option value="1">Con Parcheggio</option>
+        <option value="2">Senza Parcheggio</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2 mb-1">
+      <label for="vote">Voto</label>
+      <select id="vote" name="vote" class="form-control">
+        <option value="">Tutti</option>
+        <option value="1">2 Stelle e superiori</option>
+        <option value="2">3 Stelle e superiori</option>
+        <option value="3">4 Stelle e superiori</option>
+        <option value="4">5 Stelle</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <button type="submit" class="btn btn-primary">Filtra</button>
+    </div>
+  </form>
 
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Nome</th>
-          <th scope="col">Descrizione</th>
-          <th scope="col">Parcheggio</th>
-          <th scope="col">Voto</th>
-          <th scope="col">Distanza dal centro</th>
-        </tr>
-      </thead>
-      <tbody>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Descrizione</th>
+        <th scope="col">Parcheggio</th>
+        <th scope="col">Voto</th>
+        <th scope="col">Distanza dal centro</th>
+      </tr>
+    </thead>
+    <tbody>
 
-        <?php
-          foreach ($hotels as $index => $hotel) {
-            echo '<tr>';
-              echo '<th scope="row">'. ($index + 1) .'</th>';
-              echo '<td>' . $hotel["name"] . '</td>';
-              echo '<td>' . $hotel["description"] . '</td>';
-              echo '<td>' . ($hotel["parking"] ? 'Sì' : 'No') . '</td>';
-              echo '<td>' . $hotel["vote"] . '</td>';
-              echo '<td>' . $hotel["distance_to_center"] . 'km</td>';
-            echo '</tr>';
-          }
-        ?>
+    <!-- 
+    TEORIA
+    $filteredArray = array_filter($array, function ($element) {
+      codice che verrà verificato o meno
+    });
 
-      </tbody>
-    </table>
+    $numbers = [2, 8, 4, 10, 3, 6];
+    // Utilizza array_filter per mantenere solo i numeri maggiori di 5
+    $filteredNumbers = array_filter($numbers, function ($number) {
+        return $number > 5;
+    });
+    // $filteredNumbers conterrà solo [8, 10, 6]
+
+    isset: verifica se una variabile è stata impostata e se il suo valore non è null (in questo caso se un valore è stato selezionato dall'utente);
+    $_GET: È una superglobale in PHP che viene utilizzata per recuperare dati dalla query string dell'URL;
+    isset($_GET['x']) sta verificando se la variabile "x" è stata impostata nella query string dell'URL.
+    -->
+
+    <?php
+      $filteredHotels = $hotels;
+      
+      if (isset($_GET['parking']) && $_GET['parking'] === '1') {
+        $filteredHotels = array_filter($filteredHotels, function ($hotel) {
+            return $hotel['parking'];
+        });
+      } elseif (isset($_GET['parking']) && $_GET['parking'] === '2') {
+          $filteredHotels = array_filter($filteredHotels, function ($hotel) {
+              return !$hotel['parking'];
+          });
+      }
+    
+      if (isset($_GET['vote']) && $_GET['vote']) {
+        $filteredHotels = array_filter($filteredHotels, function ($hotel) {
+            return $hotel['vote'] > $_GET['vote'];
+        });
+      }
+
+      // Display degli hotel
+      foreach ($filteredHotels as $index => $hotel) {
+        echo "<tr>
+                <th scope='row'>" . ($index + 1) . "</th>
+                <td>{$hotel['name']}</td>
+                <td>{$hotel['description']}</td>
+                <td>" . ($hotel['parking'] ? 'Sì' : 'No') . "</td>
+                <td>{$hotel['vote']}</td>
+                <td>{$hotel['distance_to_center']} km</td>
+              </tr>";
+      }
+    ?>
+
+    </tbody>
+  </table>
   
 </body>
 </html>
